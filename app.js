09b598a5bee1aec7baa26ffe873ad07db5868426
Ajith <ajit.v.billava@gmail.com>
2020-12-05 01:00:37 +0530

    feat: added translation api

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
 var btnTransaltor=document.querySelector("#btn-translate");
+var btnTag=document.querySelector("h1");
 var txtInput=document.querySelector("#txt-input");
 
-function clickEventHandler(){
-    console.log("clicked");
-    console.log(txtInput.value);
+var outputDiv=document.querySelector("#output");
 
+
+// var serverUrl="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json" //tanay sir's server
+
+//var serverUrl="https://api.funtranslations.com/translate/yoda.json"  //yodish language
+
+var serverUrl="https://api.funtranslations.com/translate/minion.json"
+
+// outputDiv.innerText="Ajith Billava"
+
+function getTranslationUrl( txt ){
+    return serverUrl + "?"+"text="+ txt
 }
+
+function getError(error){
+    console.log("Error occured ",error)
+    alert("Something wrong with server, try again after sometime")
+}
+
+function clickEventHandler(){
+
+    var inputText=txtInput.value;
+    
+
+   fetch(getTranslationUrl(inputText))
+   .then(response => response.json())
+   .then(json => {
+       var translatedText=json.contents.translated;
+       outputDiv.innerText=translatedText;
+   })
+   .catch(getError)
+
+
+};
 
 
 btnTransaltor.addEventListener("click",clickEventHandler)
